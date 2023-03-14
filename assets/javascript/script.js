e469d42b8be6c3449cbfb7fc599d6ff464cde6a3
Grace Roh <grace@BIGMINE.attlocal.net>
2023-03-14 12:56:02 -0700

    add javascript pseudocode

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,37 @@
 
 //global variables
 
-//start game by pressing start event listener
+// question object constructor
+function quizQuestions(question, correctAnswer, optionA, optionB, optionC, optionD) {
+    this.question = question;
+    this.optionA = optionA;
+    this.optionB = optionB;
+    this.optionC = optionC;
+    this.optionD = optionD;
+    this.correctAnswer = correctAnswer
+}
 
-// question object
+//create questions
+
+//list of questions
+var toAsk = [];
+
+//start game by pressing start event listener -- starting page goes away, quiz page is shown, timer starts
+
+//timer starts
+
+//questions show up on quiz page
+
+//answer is correct -- if i answered correctly then score goes up by 10pts & correct message pops up
+
+//answer is incorrect -- if i answered incorrectly then timer goes down by 10 seconds & incorrect message pops up
+
+//when i click on view high scores, i stop seeing quiz page, and i see list of high scores
+
+//show score
+
+//game over -- stop game, show game over page
+
+//input initials & submit (event listener) --> store in local storage
+
+//display list of high scores in order of high scores
